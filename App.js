79ffc2597b69db057cdfeb87359699ed5127a7fb
Jvnko <oreoslawiet@gmail.com>
2023-06-19 17:20:45 -0700

    Instacion de paquetes

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
+import "react-native-gesture-handler";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Estamos en la pokedex</Text>
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Estamos en la pokedex</Text>
+      </View>
+    </NavigationContainer>
   );
 }
 
